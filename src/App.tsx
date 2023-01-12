1d27652ed06252069cb78f3c2f6b16d1728093bf
abaha <abaha.main@gmail.com>
2023-01-12 14:31:30 +0300

    second commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import tableImage from './images/table-square.png';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="cover">
+        <img className="img" src={tableImage}/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
